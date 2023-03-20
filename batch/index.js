ffc48b03788dd4b53fb233b89f076c95295c42fc
Robert Reinert <robert.reinert@gmail.com>
2023-03-20 15:56:19 -0700

    Added batch functions

--- batch/index.js
+++ batch/index.js
@@ -8,3 +8,23 @@ export const createBatchServiceClient = (url, account, accountKey) => {
   const batchServiceClient = new BatchServiceClient(sharedKeyCredential, url);
   return batchServiceClient;
 }
+
+export const existsBatchPool = (url, account, accountKey, poolId) => {
+  const batchServiceClient = createBatchServiceClient(url, account, accountKey);
+  return batchServiceClient.pool.exists(poolId);
+}
+
+export const getBatchPool = (url, account, accountKey, poolId) => {
+  const batchServiceClient = createBatchServiceClient(url, account, accountKey);
+  return batchServiceClient.pool.get(poolId);
+}
+
+export const addBatchJob = (url, account, accountKey, jobConfig) => {
+  const batchServiceClient = createBatchServiceClient(url, account, accountKey);
+  return batchServiceClient.job.add(jobConfig);
+}
+
+export const addBatchTask = (url, account, accountKey, jobId, taskConfig) => {
+  const batchServiceClient = createBatchServiceClient(url, account, accountKey);
+  return batchServiceClient.task.add(jobId, taskConfig);
+}
